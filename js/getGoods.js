a9fa7732808ff89eb382e1403e61a84ef05700e9
YaBuyanishe <93867987+YaBuyanishe@users.noreply.github.com>
2022-02-24 14:36:32 +0500

    added getGoods

---
+++ js/getGoods.js
@@ -0,0 +1,27 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link');
+
+    const getData = () => {
+        fetch('https://test-ab29f-default-rtdb.firebaseio.com/db.json')
+        .then(res => res.json())
+        .then(data => localStorage.setItem('data', (JSON.stringify(data))));
+    };
+    
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            getData();
+        });
+    });
+
+    // localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
+
+    // const goods = JSON.parse(localStorage.getItem('goods'));
+    // console.log(goods);
+    // console.log(localStorage);
+
+    // localStorage.removeItem('goods');
+    // console.log(localStorage);
+};
+
+getGoods();
